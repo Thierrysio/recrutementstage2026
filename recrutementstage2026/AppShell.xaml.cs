4295b5e7470083838b3987bc2dd10e394db4a40e
Thierrysio <thierry28220@icloud.com>
2026-02-02 09:04:12 +0100

    Ajoutez des fichiers projet.

---
+++ recrutementstage2026/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace recrutementstage2026
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ recrutementstage2026/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace recrutementstage2026
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
